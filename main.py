e808dcebedbfff5cc7c65e1e3766a3000bcbd682
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2020-04-13 10:12:30 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,76 @@
+from __future__ import print_function
+import time
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def main():
+    service = connect()
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    calendar_ids = []
+    while True:
+        calendar_list = service.calendarList().list().execute()
+        for calendar_list_entry in calendar_list['items']:
+            calendar_ids.append(calendar_list_entry['id'])
+        page_token = calendar_list.get('nextPageToken')
+        if not page_token:
+            break
+
+    closest = [-1,""]
+    for id in calendar_ids:
+        events_result = service.events().list(calendarId=id, timeMin=now,
+                                            maxResults=10, singleEvents=True,
+                                            orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            continue
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            summary = event['summary']
+            if "T" in start:
+                current = time.mktime(datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S%z').timetuple())
+                if closest[0] == -1 or current < closest[0]:
+                    closest[0] = current
+                    closest[1] = summary
+
+    print("next: " + closest[1])
+
+def connect():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+    return service
+
+
+if __name__ == '__main__':
+    main()
