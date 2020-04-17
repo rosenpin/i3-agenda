3b01841da5a1523842481bde4d68fc9a1861b33d
Martin Brylski <martin.brylski@staffbase.com>
2020-04-17 09:38:53 +0200

    changed token and cache path to users directory

--- i3_agenda/__init__.py
+++ i3_agenda/__init__.py
@@ -1,34 +1,52 @@
 #!/usr/bin/env python3
 
 from __future__ import print_function
-import json
-from bidi.algorithm import get_display
-import time
-import datetime
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-import argparse
-import os
-import subprocess
 
+import argparse
+import datetime
+import json
+import os
+import os.path
+import pickle
+import subprocess
+import time
+from os.path import expanduser
+
+from bidi.algorithm import get_display
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+CONF_DIR = expanduser('~') + os.path.sep + ".i3agenda"
+TMP_TOKEN = f"{CONF_DIR}/i3agenda_google_token.pickle"
+CACHE_PATH = f"{CONF_DIR}/i3agenda_cache.txt"
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-TMP_TOKEN = '/tmp/i3agenda_google_token.pickle'
-CACHE_PATH = '/tmp/i3agenda_cache.txt'
 DEFAULT_CAL_WEBPAGE = 'https://calendar.google.com/calendar/r/day'
 
-button = os.getenv("BLOCK_BUTTON", "") # i3blocks use this envvar to check the click
+button = os.getenv("BLOCK_BUTTON",
+                   "")  # i3blocks use this envvar to check the click
 
 parser = argparse.ArgumentParser(description='Show next Google Calendar event')
-parser.add_argument('--credentials', '-c', type=str,
+parser.add_argument('--credentials',
+                    '-c',
+                    type=str,
                     default='',
                     help='path to your credentials.json file')
-parser.add_argument('--cachettl', '-ttl', type=int, default=30,
-                   help='time for cache to be kept in minutes')
-parser.add_argument('--ids', '-i', type=str, default=[], nargs='+',
-                    help='list of calendar ids to fetch, space separated. If none is specified all calendars will be fetched')
+parser.add_argument('--cachettl',
+                    '-ttl',
+                    type=int,
+                    default=30,
+                    help='time for cache to be kept in minutes')
+parser.add_argument(
+    '--ids',
+    '-i',
+    type=str,
+    default=[],
+    nargs='+',
+    help=
+    'list of calendar ids to fetch, space separated. If none is specified all calendars will be fetched'
+)
+
 
 class Event():
     def __init__(self, summary, is_allday, unix_time, end_time):
@@ -37,12 +55,14 @@ class Event():
         self.unix_time = unix_time
         self.end_time = end_time
 
+
 class EventEncoder(json.JSONEncoder):
-   def default(self, o): # pylint: disable=E0202
-    if isinstance(o, Event):
-        return o.__dict__
-    else:
-        return json.JSONEncoder.default(self, o)
+    def default(self, o):  # pylint: disable=E0202
+        if isinstance(o, Event):
+            return o.__dict__
+        else:
+            return json.JSONEncoder.default(self, o)
+
 
 def main():
     args = parser.parse_args()
@@ -51,7 +71,7 @@ def main():
 
     if button != "":
         subprocess.Popen(["xdg-open", DEFAULT_CAL_WEBPAGE])
-        
+
     events = load_cache(args.cachettl)
     if events == None:
         service = connect(args.credentials)
@@ -62,17 +82,20 @@ def main():
     if closest is None:
         print("No events")
         return
-        
+
     t = datetime.datetime.fromtimestamp(closest.unix_time)
     print(f"{t:%H:%M} " + get_display(closest.summary))
 
+
 def getEvents(service, allowed_calendars_ids):
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    now = datetime.datetime.utcnow().isoformat(
+    ) + 'Z'  # 'Z' indicates UTC time
     calendar_ids = []
     while True:
         calendar_list = service.calendarList().list().execute()
         for calendar_list_entry in calendar_list['items']:
-            if not allowed_calendars_ids or calendar_list_entry['id'] in allowed_calendars_ids:
+            if not allowed_calendars_ids or calendar_list_entry[
+                    'id'] in allowed_calendars_ids:
                 calendar_ids.append(calendar_list_entry['id'])
         page_token = calendar_list.get('nextPageToken')
         if not page_token:
@@ -80,39 +103,48 @@ def getEvents(service, allowed_calendars_ids):
 
     all = []
     for id in calendar_ids:
-        events_result = service.events().list(calendarId=id, timeMin=now,
-                                            maxResults=10, singleEvents=True,
-                                            orderBy='startTime').execute()
+        events_result = service.events().list(calendarId=id,
+                                              timeMin=now,
+                                              maxResults=10,
+                                              singleEvents=True,
+                                              orderBy='startTime').execute()
         events = events_result.get('items', [])
 
         if not events:
             continue
 
         for event in events:
-            end_time = get_event_time(event['end'].get('dateTime', event['end'].get('date')))
-            start_time = event['start'].get('dateTime', event['start'].get('date'))
+            end_time = get_event_time(event['end'].get(
+                'dateTime', event['end'].get('date')))
+            start_time = event['start'].get('dateTime',
+                                            event['start'].get('date'))
             unix_time = get_event_time(start_time)
-            all.append(Event(event['summary'], is_allday(start_time), unix_time, end_time))
+            all.append(
+                Event(event['summary'], is_allday(start_time), unix_time,
+                      end_time))
 
     return all
+
 
 def is_allday(start_time) -> bool:
     return "T" not in start_time
 
+
 def get_event_time(full_time):
     if "T" in full_time:
         format = '%Y-%m-%dT%H:%M:%S%z'
-    else: 
+    else:
         format = '%Y-%m-%d'
 
     # Python introduced the ability to parse ":" in the timezone format (in strptime()) only from version 3.7 and up.
     # We need to remove the : before the timezone to support older versions
     # See https://stackoverflow.com/questions/30999230/how-to-parse-timezone-with-colon for more information
     if full_time[-3] == ":":
-        full_time = full_time[:-3]+full_time[-2:]
+        full_time = full_time[:-3] + full_time[-2:]
 
-    return time.mktime(datetime.datetime.strptime(full_time,
-                                                  format).astimezone().timetuple())
+    return time.mktime(
+        datetime.datetime.strptime(full_time, format).astimezone().timetuple())
+
 
 def get_closest(events) -> Event:
     closest = None
@@ -130,6 +162,7 @@ def get_closest(events) -> Event:
 
     return closest
 
+
 def load_cache(cachettl):
     if not os.path.exists(CACHE_PATH):
         return None
@@ -143,34 +176,41 @@ def load_cache(cachettl):
         with open(CACHE_PATH, 'r') as f:
             raw = json.loads(f.read())
             for event in raw:
-                events.append(Event(event['summary'], event['is_allday'], event['unix_time'], event['end_time']))
+                events.append(
+                    Event(event['summary'], event['is_allday'],
+                          event['unix_time'], event['end_time']))
         return events
     except Exception:
         # Invalid cache
         return None
 
+
 def save_cache(events):
     with open(CACHE_PATH, 'w+') as f:
         f.write(EventEncoder().encode(events))
+
 
 def connect(credspath):
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
+    if not os.path.exists(CONF_DIR):
+        os.mkdir(CONF_DIR)
     if os.path.exists(TMP_TOKEN):
         with open(TMP_TOKEN, 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if not os.path.exists(credspath):
-            print("You need to download your credentials json file from the Google API Console and pass its path to this script")
+            print(
+                "You need to download your credentials json file from the Google API Console and pass its path to this script"
+            )
             exit(1)
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                credspath, SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credspath, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open(TMP_TOKEN, 'wb') as token:
@@ -178,6 +218,7 @@ def connect(credspath):
 
     service = build('calendar', 'v3', credentials=creds)
     return service
+
 
 if __name__ == '__main__':
     main()
