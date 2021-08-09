c9a1d7917f37838c1a24b0bfd4713fc040bdc19f
IgorFroehner <igoor.sf14@gmail.com>
2021-08-09 14:57:09 -0300

    refactor: taking events from cache to a separated function

--- i3_agenda/cache_utils.py
+++ i3_agenda/cache_utils.py
@@ -1,6 +1,6 @@
 
 from config import CONF_DIR
-from typing import Optional, List
+from typing import Optional, List, TextIO
 
 import os.path
 import time
@@ -18,28 +18,31 @@ def load_cache(cachettl: int) -> Optional[List[Event]]:
     if time.time() - os.path.getmtime(CACHE_PATH) > cachettl * 60:
         return None
 
-    events = []
-
     try:
         with open(CACHE_PATH, "r") as f:
-            raw = json.loads(f.read())
-            for event in raw:
-                events.append(
-                    Event(
-                        event["summary"],
-                        event["is_allday"],
-                        event["unix_time"],
-                        event["end_time"],
-                        event["location"],
-                    )
-                )
+            events = get_events_from_cache(f)
         return events
     except IOError:
         # Invalid cache
         return None
 
 
-def save_cache(events: List[Event]) -> None:
+def save_cache(events: List[Event]):
     with open(CACHE_PATH, "w+") as f:
         f.write(EventEncoder().encode(events))
 
+
+def get_events_from_cache(f: TextIO) -> List[Event]:
+    events = []
+    raw = json.loads(f.read())
+    for event in raw:
+        events.append(
+            Event(
+                event["summary"],
+                event["is_allday"],
+                event["unix_time"],
+                event["end_time"],
+                event["location"],
+            )
+        )
+    return events
