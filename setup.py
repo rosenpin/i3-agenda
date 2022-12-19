970c92690cbd3e80e9db5068e921b6edc4049772
matclab <mathieu.clabaut@gmail.com>
2022-12-19 15:00:46 +0100

    CD auto pypi upload (#87) (#91)
    * Replace dash with underscore in filename (needed for python -m build)
    
    * Add publish workflow
    
    * gh actions: run tests on PR event
    
    * Make comment_on_pr working (hopefully)
    
    Co-authored-by: matclab <mathieu@clabaut.net>

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     ],
     entry_points={
         "console_scripts": [
-            "i3-agenda = i3_agenda.i3-agenda:main",
+            "i3-agenda = i3_agenda.i3_agenda:main",
         ],
     },
     python_requires=">=3.3",
